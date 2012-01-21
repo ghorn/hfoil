dd89e646490acb77182b997b778a755c217afd0f
Greg Horn <gregmainland@gmail.com>
2012-01-21 01:56:46 -0800

    preliminary 2d potential solver working

---
+++ hw1/airfoil_solver.py
@@ -0,0 +1,196 @@
+from numpy import *
+import matplotlib.pyplot as pylab
+#from scipy.interpolate import interp1d
+#from streamplot import streamplot
+
+def thicknessY(tau, xc):
+    # tau = thickness/chord
+    # xc = x/c
+    return 5*tau*(0.2969*sqrt(xc) - 0.1260*(xc) - 0.3537*(xc)**2 + 0.2843*(xc)**3 - 0.1015*(xc)**4)
+
+def sourceU(strength, x, y):
+    r2 = x*x + y*y
+    return strength/(2*pi) * array([x/r2, y/r2])
+
+class Panel():
+    def __init__(self, position0, position1):
+        self._position0 = array(position0)
+        self._position1 = array(position1)
+
+    def centerPos(self):
+        return 0.5*(self._position0 + self._position1)
+
+    def normal(self):
+        dxy = self._position1 - self._position0
+        normal = array([-dxy[1], dxy[0]])
+        return normal / sqrt(dot(normal,normal))
+
+    def length(self):
+        dx = self._position0[0] - self._position1[0]
+        dy = self._position0[1] - self._position1[1]
+        return sqrt(dx*dx + dy*dy)
+
+class Vortex():
+    def __init__(self, position, gamma = 0):
+        self._gamma = gamma
+        self._position = array(position)
+
+    def vel(self, testPos):
+        return self.velPerGamma(testPos)*self._gamma
+
+    def velPerGamma(self, testPosition):
+        dx = testPosition[0] - self._position[0]
+        dy = testPosition[1] - self._position[1]
+        r2 = dx*dx + dy*dy
+        return array([-dy/r2, dx/r2]) / (2*pi)
+
+class Source():
+    def __init__(self, position, gamma = 0):
+        self._gamma = gamma
+        self._position = array(position)
+
+    def vel(self, testPos):
+        return self.velPerGamma(testPos)*self._gamma
+
+    def velPerGamma(self, testPosition):
+        dx = testPosition[0] - self._position[0]
+        dy = testPosition[1] - self._position[1]
+        r2 = dx*dx + dy*dy
+        return array([dx/r2, dy/r2]) / (2*pi)
+
+
+class Flow():
+    def __init__(self, uinf, nVortices = 5):
+        self.uinf = uinf
+        xs = linspace(0.01, 0.95, nVortices)
+        self.vortices = [Vortex((x,0)) for x in xs]
+        self.vortices += [Source((x,0)) for x in xs]
+
+    def dUdGamma(self, panels):
+        xMat = mat([[vort.velPerGamma(panel.centerPos())[0] for vort in self.vortices] for panel in panels])
+        yMat = mat([[vort.velPerGamma(panel.centerPos())[1] for vort in self.vortices] for panel in panels])
+        return vstack((xMat, yMat))
+
+    def solveGammas(self, panels):
+        np = len(panels)
+        uinf = vstack((mat(ones((np,1)))*self.uinf[0], mat(ones((np,1)))*self.uinf[1]))
+
+        A = self.dUdGamma(panels)
+
+        nxs = [p.length()*p.normal()[0] for p in panels]
+        nys = [p.length()*p.normal()[1] for p in panels]
+        N = mat(hstack((diag(nxs),diag(nys))))
+
+        NA = dot(N,A)
+#        print linalg.matrix_rank(A)
+#        print linalg.matrix_rank(dot(N,A))
+        piNA = dot( linalg.inv(dot(NA.T,NA)), NA.T)
+        piNA = linalg.pinv(NA)
+#        print linalg.pinv(NA) - piNA
+        Nuinf = dot(N,uinf)
+        gammas = dot(piNA, -Nuinf)
+#        gammas = linalg.lstsq(NA, -Nuinf)[0]
+
+        for vort,gamma in zip(self.vortices,gammas):
+            vort._gamma = gamma[0,0]
+
+        # display error:
+#        print dot(NA, gammas) + dot(N, uinf)
+
+    def plot(self, thicknessYFun):
+        xs = []
+        ys = []
+        us = []
+        vs = []
+        for x in linspace(-0.2,1.2,30):
+            for y in linspace(-0.4,0.4, 25):
+                if abs(y) > thicknessYFun(x):
+                    xs.append(x)
+                    ys.append(y)
+                    us.append(sum([vort.vel([x,y])[0] for vort in self.vortices] ) + self.uinf[0])
+                    vs.append(sum([vort.vel([x,y])[1] for vort in self.vortices] ) + self.uinf[1])
+        pylab.quiver(xs,ys,us,vs)
+
+#        xs = array(xs)
+#        ys = array(ys)
+
+#        us = array( sum([vort.vel([xs,ys[:,newaxis]])[0] for vort in self.vortices] ) + self.uinf[0] )
+#        vs = array( sum([vort.vel([xs,ys[:,newaxis]])[0] for vort in self.vortices] ) + self.uinf[0] )
+#        streamplot(xs,ys,us,vs)
+
+        pylab.plot([v._position[0] for v in self.vortices], [v._position[1] for v in self.vortices], 'rx')
+#        for v in self.vortices:
+#            print v._gamma
+
+
+class Airfoil():
+
+    def __init__(self, tau=0.12, chord=1.0, nPanels=6):
+        self.tau = tau
+        self.chord = chord
+
+        self.nPanels = nPanels
+        # make number of panels even by adding one if odd
+        if self.nPanels % 2 == 1:
+            self.nPanels += 1
+            print "setting number of panels to " + str(self.nPanels) + " to make them even"
+    
+        self.nChordPoints = self.nPanels/2 + 1
+        #print "nChordPoints: " + str(self.nChordPoints)
+        xcs = linspace(0,1,self.nChordPoints)
+    
+    #    xcsLeading  = xcs[0]
+    #    xcsTrailing = xcs[-1]
+        xcsTop      = xcs[1:-1]
+        xcsBottom   = xcsTop[::-1]
+    
+        positions = [[0,0]] + \
+                    [[chord*xc,  self.halfThickness(chord*xc)] for xc in xcsTop] + \
+                    [[chord, 0]] + \
+                    [[chord*xc, -self.halfThickness(chord*xc)] for xc in xcsBottom]
+
+        self.panels = []
+        for k in range(len(positions)-1):
+            self.panels.append(Panel(positions[k], positions[k+1]))
+        self.panels.append(Panel(positions[len(positions)-1], positions[0]))
+
+    def halfThickness(self, x):
+        if x < 0 or x > self.chord:
+            return 0.0
+
+        xc = x/self.chord
+        return self.chord*thicknessY(self.tau, xc)
+
+    def plot(self):
+        xs = [p._position0[0] for p in self.panels + [self.panels[0]]]
+        ys = [p._position0[1] for p in self.panels + [self.panels[0]]]
+        pylab.plot(xs,ys,'-')
+
+        xcs = [p.centerPos()[0] for p in self.panels + [self.panels[0]]]
+        ycs = [p.centerPos()[1] for p in self.panels + [self.panels[0]]]
+        ucs = [p.normal()[0] for p in self.panels + [self.panels[0]]]
+        vcs = [p.normal()[1] for p in self.panels + [self.panels[0]]]
+#        pylab.quiver(xcs,ycs,ucs,vcs,scale=15)
+
+uinf = 20
+alpha = 5*pi/180
+uinf = [cos(alpha)*uinf, sin(alpha)*uinf]
+
+foil = Airfoil(tau=0.12, chord=1.0, nPanels=200)
+flow = Flow(uinf, nVortices=50)
+
+flow.solveGammas(foil.panels)
+
+foil.plot()
+flow.plot(lambda x: foil.halfThickness(x))
+
+pylab.axis('equal')
+pylab.show()
+
+#xcs = linspace(0,1,100)
+#T = thicknessY(0.12,xcs)
+#
+#pylab.plot(xcs,T)
+#pylab.plot(xcs,-T)
+#pylab.axis('equal')
+#pylab.show()
